c42c9fce966ff7eeaf28d7742ab444567b516691
Mia Charifson <miacharifson@Mias-Air.lan>
2024-01-17 17:56:14 -0500

    investigating LTFU groups and addresses outside NYC

--- Code/3.01_clean_address_data.R
+++ Code/3.01_clean_address_data.R
@@ -58,7 +58,22 @@ length(unique(res_his_unique$pat_id))
 
 write.csv(res_his_unique, 'Data/Derived/all_cohort_addresses.csv')
 
-# how many people move at least once
+## explore the distribution of census tracts
+table(substr(res_his_unique$full_fips_tract_final, 1, 2))
+# the most populous are NY, NJ, PA, CT and to a lesser extent CA, MA, MD, DC and GA
+length(which(substr(res_his_unique$full_fips_tract_final, 1, 2)!=36))
+length(unique(res_his_unique$pat_id[which(substr(res_his_unique$full_fips_tract_final, 1, 2)!=36)]))
+# look at only NYC counties
+nyc_counties <- c(36005, 36047, 36061, 36081, 36085)
+table(substr(res_his_unique$full_fips_tract_final, 1, 5))
+# filter to only those with a nyc county fips
+length(unique(res_his_unique$pat_id[which(substr(res_his_unique$full_fips_tract_final, 1, 5) %in% nyc_counties)]))
+# 84979 patients
+
+################################################################################
+# Look for baseline address #####################################################
+################################################################################
+
 res_his_unique <- left_join(res_his_unique, pat_cohort[c("pat_id", "index_enc_date")], by="pat_id")
 res_his_baseline <- res_his_unique %>%
   mutate(baseline_addr = case_when(index_enc_date < eff_end_date_max & index_enc_date >= eff_start_date_min~TRUE, 
@@ -97,9 +112,56 @@ res_his_baseline_unique %>% group_by(pat_id) %>% filter(n()>1) %>% arrange(pat_i
 res_his_baseline_unique %>% group_by(pat_id) %>% filter(n()>1 & n_distinct(census_tract_final)>1) %>% arrange(pat_id, eff_start_date_min) %>% View()
 length(which(res_his_baseline_unique$mean_accuracy_score < 0.8))/dim(res_his_baseline_unique)[1] #3% are low accuracy
 
-# write out the file
-write.csv(res_his_baseline, 'Data/Derived/baseline_cohort_addresses.csv')
-
 ## explore the distribution of census tracts
-length(which(substr(res_his_baseline$full_fips_tract_final, 1, 2) != 36))
-length(which(res_his_baseline$pat_id[which(substr(res_his_baseline$full_fips_tract_final, 1, 2) != 36)]))
+table(substr(res_his_baseline_unique$full_fips_tract_final, 1, 2))
+# the most populous are NY, NJ, PA, CT and to a lesser extent CA, MA, MD, DC and GA
+length(which(substr(res_his_baseline_unique$full_fips_tract_final, 1, 2)!=36))
+length(unique(res_his_baseline_unique$pat_id[which(substr(res_his_baseline_unique$full_fips_tract_final, 1, 2)!=36)]))
+# around 14911 are not from NY
+# look at only NYC counties
+table(substr(res_his_baseline_unique$full_fips_tract_final, 1, 5))
+# filter to only those with a nyc county fips
+length(unique(res_his_baseline_unique$pat_id[which(substr(res_his_baseline_unique$full_fips_tract_final, 1, 5) %in% nyc_counties)]))
+# 81256 patients
+ids_nyc <- unique(res_his_baseline_unique$pat_id[which(substr(res_his_baseline_unique$full_fips_tract_final, 1, 5) %in% nyc_counties)])
+
+## for those who do not have a baseline address in NYC but have any address in NYC look at them
+not_nyc_baseline <- res_his_baseline %>%
+  filter(!pat_id %in% ids_nyc) %>%
+  mutate(nyc_addr = ifelse(substr(full_fips_tract_final, 1, 5) %in% nyc_counties, TRUE, FALSE)) %>%
+  arrange(pat_id, eff_start_date_min, eff_end_date_max) %>%
+  group_by(pat_id) %>%
+  mutate(ever_nyc = ifelse(sum(nyc_addr)>=1, TRUE, FALSE))
+# how many are not a nyc patient
+length(unique(not_nyc_baseline$pat_id)) #22997
+# how many have at least one nyc address
+length(unique(not_nyc_baseline$pat_id[which(not_nyc_baseline$ever_nyc)])) #3723
+# look at these patients and determine if any of them might be eligible (i.e. mostly NYC but edge case)
+not_nyc_baseline %>% 
+  filter(ever_nyc) %>% 
+  select(pat_id, index_enc_date, baseline_addr, nyc_addr, contains('eff'), full_fips_tract_final) %>% 
+  View()
+
+# look for instances where their nyc address is before the index encounter and the margin to index encounter
+nyc_before_baseline <- not_nyc_baseline %>%
+  group_by(pat_id) %>%
+  filter(nyc_addr & eff_end_date_max <= index_enc_date) 
+summary(as.numeric(nyc_before_baseline$addr_time_margin))
+length(which(as.numeric(nyc_before_baseline$addr_time_margin)<30)) #363 are less than a month
+## how long were they at this NYC address
+nyc_before_baseline %>%
+  filter(addr_time_margin<30) %>%
+  mutate(length_residence = as.numeric(difftime(eff_end_date_max, eff_start_date_min, unit='days'))) %>%
+  summary()
+# the shortest length of residence is 0 days, the longest 8 years
+# lets look at the short ones
+nyc_before_baseline %>%
+  filter(addr_time_margin<30) %>%
+  mutate(length_residence = as.numeric(difftime(eff_end_date_max, eff_start_date_min, unit='days'))) %>%
+  filter(length_residence < 365) %>%
+  View()
+# I might want to look at the average length of residence up until index encounter for those who are in NYC addresses
+
+# write out the file
+write.csv(res_his_baseline_unique, 'Data/Derived/baseline_cohort_addresses.csv')
+
